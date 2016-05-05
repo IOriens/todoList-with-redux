bf134b5868ed4d66d7693b1d2bdd253492c31763
IOriens <djjoriens@gmail.com>
2016-05-06 00:44:41 +0800

    update

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+var path = require('path')
+var webpack = require('webpack')
+module.exports = {
+    watch: true,
+    entry: {
+        'index': 'index',
+        'vendor': ['react', 'redux', 'react-redux']
+    },
+    output: {
+        path: path.join(__dirname, 'public/js/dest'),
+        filename: '[name].bundle.js'
+    },
+    module: {
+        loaders: [{
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel',
+            query: {
+                presets: ['es2015', 'react']
+            }
+        }]
+    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin("vendor", "vendor.bundle.js"),
+        // new webpack.optimize.UglifyJsPlugin({
+        //     compress: {
+        //         warnings: false
+        //     }
+        // })
+    ],
+    resolve: {
+        extensions: ["", ".js", ".jsx", '.es6'],
+        root: path.join(__dirname, '/public/js/src/'),
+        modulesDirectories: ["node_modules"]
+    }
+}
